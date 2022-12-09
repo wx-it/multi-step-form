43b7ff14fca87b25132da2bfe5218028a4344f10
Lix <zinebbendjafer9@gmail.com>
2022-12-09 19:37:38 +0100

    Update Step2.jsx

--- src/components/Step2.jsx
+++ src/components/Step2.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 const Step2 = () => {
   return (
-  <div className='border-2 border-black'>    
+  <div className=''>    
   <div className='bg-White py-8 px-4 rounded-lg shadow-xl'>
     
-  <h2>Select your plan</h2>
-  <p>You have the option of monthly or yearly billing.</p>
+  <h2
+    className='text-Marine-blue text-xl font-bold'
+  >
+    Select your plan</h2>
+  <p
+   className="text-Cool-gray w-72 my-1"
+  >You have the option of monthly or yearly billing.</p>
 
 <div>
     <h3>Arcade</h3>
@@ -28,7 +33,9 @@ const Step2 = () => {
 
 <div>
     <button>Go Back</button>
-    <button>Next Step</button>
+    <button
+       className='border-2 bg-Marine-blue text-White text-bold px-4 py-2 rounded-md my-4 font-normal text-ms'
+    >Next Step</button>
 </div>
 
   </div>
