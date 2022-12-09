2dea01a3b5ababa3d1932f0249812f92a7904577
Lix <zinebbendjafer9@gmail.com>
2022-12-09 19:28:04 +0100

    Update Step1.jsx

--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -2,25 +2,53 @@ import React from 'react'
 
 const Step1 = () => {
   return (
-    <div className="border-2 border-black">
+    <div className="">
    <div className='bg-White py-8 px-4 rounded-lg shadow-xl'>
    <div>
         <h1
-        className='text-Marine-blue text-3xl'
+        className='text-Marine-blue text-xl font-bold'
         >Personal info</h1>
          <p
-         className="text-Cool-gray"
+         className="text-Cool-gray w-72 my-1"
          >Please provide your name, email address, and phone number.</p>
     </div>
-  <label htmlFor="">Name</label>
-  <input type="text"  placeholder='e.g. Stephen King'/>
+  <div className="flex flex-col my-5">
+  <label
+   htmlFor=""
+   className='text-sm font-medium text-Marine-blue '
+   >Name</label>
+  <input
+   type="text" 
+   placeholder='e.g. Stephen King'
+   className='border border-Light-gray py-2 px-3 pl-5 font-normal rounded text-Cool-gray'
+   />
+  </div>
+  <div className='flex flex-col my-5'>
+    
+  <label
+   htmlFor=""
+   className='text-sm font-medium text-Marine-blue '
+   >Email Address</label>
+  <input
+   type="text"
+   placeholder="e.g. stephenking@lorem.com"
+   className='border border-Light-gray py-2 px-3 pl-5 font-normal rounded text-Cool-gray'
+   />
   
-  <label htmlFor="">Email Address</label>
-  <input type="text" placeholder="e.g. stephenking@lorem.com"/>
+  </div>
   
-  <label htmlFor="">Phone Number</label>
-  <input type="text" placeholder="e.g. +1 234 567 890"/>
+  <div className='flex flex-col my-5'>
+  <label
+   htmlFor=""
+   className='text-sm font-medium text-Marine-blue '
+   >Phone Number</label>
+  <input
+   type="text"
+   placeholder="e.g. +1 234 567 890"
+   className='border border-Light-gray py-2 px-3 pl-5 font-normal rounded text-Cool-gray'
+   />
   
+  </div>
    </div>
    
      <div>
