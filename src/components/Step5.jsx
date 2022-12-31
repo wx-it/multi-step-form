76c69bb489bea5b11ccc50d173edd8dbc1a988e2
Lix <zinebbendjafer9@gmail.com>
2022-12-31 19:11:23 +0100

    step 5 desktop styling done

--- src/components/Step5.jsx
+++ src/components/Step5.jsx
@@ -2,11 +2,11 @@ import iconThankYou from '../assets/images/icon-thank-you.svg'
 
 const Step5 = () => {
   return (
-    <div className='rounded-lg shadow-xl flex flex-col items-center justify-evenly space-y-5 py-14 bg-White '>
-      <img className='w-16' src={iconThankYou} alt="" />
-      <h3 className='text-Marine-blue text-xl font-bold'>Thank you!</h3>
+    <div className='rounded-lg shadow-xl flex flex-col items-center justify-evenly space-y-5 py-14 bg-White md:shadow-none '>
+      <img className='w-16 md:w-20' src={iconThankYou} alt="" />
+      <h3 className='text-Marine-blue text-xl font-bold md:text-xxl'>Thank you!</h3>
 
-<p className='text-center text-Cool-gray px-4'>
+<p className='text-center text-Cool-gray px-4 md:w-[480px] '>
   Thanks for confirming your subscription! We hope you have fun 
   using our platform. If you ever need support, please feel free 
   to email us at support@loremgaming.com.
